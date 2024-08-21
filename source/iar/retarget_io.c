792d62b277807a4721343faa405efdd000a60bda
Robin Kastberg <robin.kastberg@iar.com>
2024-08-21 16:48:54 +0200

    preliminary file redirection support for IAR

--- source/iar/retarget_io.c
+++ source/iar/retarget_io.c
@@ -17,8 +17,13 @@
  */
 
 #include <stdio.h>
+#include <errno.h>
 
 #include "RTE_Components.h"
+
+#ifdef RTE_CMSIS_Compiler_File_Interface
+#include "retarget_fs.h"
+#endif
 
 #ifdef RTE_CMSIS_Compiler_STDERR
 #include "retarget_stderr.h"
@@ -32,7 +37,7 @@
 #include "retarget_stdout.h"
 #endif
 
-#if defined(RTE_CMSIS_Compiler_STDIN)
+#if defined(RTE_CMSIS_Compiler_STDIN) || defined(RTE_CMSIS_Compiler_File_Interface)
 
 size_t __read(int handle, unsigned char *buf, size_t bufSize)
 {
@@ -53,12 +58,21 @@ size_t __read(int handle, unsigned char *buf, size_t bufSize)
   }
 #endif
 
-  return -1;
+#if defined(RTE_CMSIS_Compiler_File_Interface)
+  sz = rt_fs_read(fildes, buf, nbyte);
+  if (sz < 0) {
+    sz = -1;
+  }
+  return (sz);
+#else
+  /* Not implemented */
+  return (-1);
+#endif
 }
 
 #endif
 
-#if defined(RTE_CMSIS_Compiler_STDOUT) || defined(RTE_CMSIS_Compiler_STDERR)
+#if defined(RTE_CMSIS_Compiler_STDOUT) || defined(RTE_CMSIS_Compiler_STDERR) || defined(RTE_CMSIS_Compiler_File_Interface)
 
 size_t __write(int handle, const unsigned char *buf, size_t bufSize)
 {
@@ -88,7 +102,91 @@ size_t __write(int handle, const unsigned char *buf, size_t bufSize)
     return bufSize;
   }
 #endif
-  return -1;
+
+
+#if defined(RTE_CMSIS_Compiler_File_Interface)
+  sz = rt_fs_write(fildes, buf, nbyte);
+  if (sz < 0) {
+    sz = -1;
+  }
+  return (sz);
+#else
+  /* Not implemented */
+  errno = EDOM;
+  return (-1);
+#endif
 }
 
 #endif
+
+
+int  __close(int fildes)
+{
+  switch (fildes) {
+    case 0:
+    case 1:
+    case 2:
+      return (0);
+  }
+
+#if defined(RTE_CMSIS_Compiler_File_Interface)
+  int32_t rval;
+  rval = rt_fs_close(fildes);
+  if (rval < 0) {
+    errno = rval;
+    rval = -1;
+  }
+  return (rval);
+#else
+  /* Not implemented */
+  errno = EDOM;
+  return (-1);
+#endif
+}
+
+
+int  __open(const char *path, int oflag)
+{
+#if defined(RTE_CMSIS_Compiler_File_Interface)
+  mode  = oflag & (O_RDONLY | O_WRONLY | O_RDWR | O_APPEND);
+  mode |= (oflag & (O_CREAT | O_TRUNC)) >> 1;
+  rval = rt_fs_open(path, mode);
+  if (rval < 0) {
+    errno = rval;
+    rval = -1;
+  }
+  return (rval);
+#else
+  /* Not implemented */
+  (void)path;
+  (void)oflag;
+  errno = EDOM;
+  return (-1);
+#endif
+}
+
+long          __lseek(int fildes, long offset, int whence)
+{
+#if defined(RTE_CMSIS_Compiler_File_Interface)
+  rval = rt_fs_seek(fildes, offset, whence);
+  if (rval < 0) {
+    errno = (int)rval;
+    rval = -1;
+  }
+  else {
+    if ((sizeof(off_t) != sizeof(int64_t)) && ((rval >> 32) != 0)) {
+      /* Returned file offset does not fit into off_t */
+      rval = -1;
+      errno = EOVERFLOW;
+    }
+  }
+  return ((off_t)rval);
+#else
+  /* Not implemented */
+  (void)offset;
+  (void)whence;
+  errno = EDOM;
+  return (-1);
+#endif
+}
+
